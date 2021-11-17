55e77d6c1fe4f651f92c8505587b0061cf0e151f
Hasan Saju <hasansaju11@gmail.com>
2021-11-17 11:22:32 +0600

    Setup Complete

---
+++ my-burger/src/Components/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,12 @@
+// logic handling
+import React, { Component } from "react";
+
+export default class BurgerBuilder extends Component {
+  render() {
+    return (
+      <div>
+        <p>Burger Builder</p>
+      </div>
+    );
+  }
+}
